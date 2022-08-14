096a7f3c3775a32fa24209f4c9996e99bf787983
aryan19461 <aryansingh19461@gmail.com>
2022-08-15 00:22:24 +0530

    Independece Day - Indian Flag animation

---
+++ src/Flag.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./index.css";
+const Flag = () => {
+  return (
+    <>
+      <div className="greet">
+        <h3>HAPPY INDEPENDENCE DAY</h3>
+      </div>
+      <div className="upper"></div>
+      <div className="mid">
+        <img
+          src="https://ih1.redbubble.net/image.2642072884.8718/st,small,507x507-pad,600x600,f8f8f8.jpg"
+          alt="chakra"
+          className="chakra"
+        />
+      </div>
+      <div className="lower"></div>
+    </>
+  );
+};
+
+export default Flag;
